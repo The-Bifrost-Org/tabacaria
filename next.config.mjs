bc7fd5b4e52ed3d3017e77f71e9e44154414cfca
Vinicius Victor <vine.victordev@gmail.com>
2026-05-27 01:22:09 -0300

    feat(config): habilita imagens remotas e ignora variáveis de ambiente

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*.public.blob.vercel-storage.com',
+            },
+        ],
+    },
 }
 
 export default nextConfig
