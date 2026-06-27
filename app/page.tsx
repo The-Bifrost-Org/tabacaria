767781a14c4ff4142c810a8d94cbbc35b314964d
JvCezarino <joaocezarino2001@gmail.com>
2026-06-26 23:40:06 -0300

    Att LandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function LandingPage() {
       <HeroBanner />
       <FeaturedProducts />
       <FlashSaleSection />
-      {/* <Brands /> */}
+      {<Brands />}
       <StoreMap />
     </main>
   );
